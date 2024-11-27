ab90c5c8af9e1d756c6e160035dac53ec6dec0e1
Jeff Weber <jweber@cofront.net>
2024-11-27 12:16:58 -0500

    day 2 part 2

--- 2019/day02/src/part2.rs
+++ 2019/day02/src/part2.rs
@@ -1,5 +1,31 @@
-pub fn process(_input: &str) -> anyhow::Result<String> {
-    todo!("process");
+use aoc::intcode::execute_program;
+
+pub fn process(input: &str) -> anyhow::Result<String> {
+    let program = input
+        .lines()
+        .flat_map(|line| line.split(","))
+        .map(|item| {
+            item.parse::<usize>()
+                .expect(&format!("not a number {:?}", item))
+        })
+        .collect::<Vec<_>>();
+
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut check = program.clone();
+            check[1] = noun;
+            check[2] = verb;
+
+            let result = execute_program(check)?;
+            if result[0] == 19690720 {
+                println!("{} {}", noun.to_string(), verb.to_string());
+                let rt = 100 * noun + verb;
+                return Ok(rt.to_string());
+            }
+        }
+    }
+
+    Err(anyhow::anyhow!("no solution found"))
 }
 
 #[cfg(test)]
