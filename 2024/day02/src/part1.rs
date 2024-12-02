795e44c556bdf42558f1b0963655c2cac726596e
Jeff Weber <jweber@cofront.net>
2024-12-02 14:22:00 -0500

    day 2 part 1 works, 2 not

--- 2024/day02/src/part1.rs
+++ 2024/day02/src/part1.rs
@@ -1,5 +1,84 @@
-pub fn process(_input: &str) -> anyhow::Result<String> {
-    Ok(String::from("part 1"))
+use std::cmp::PartialEq;
+
+#[derive(Debug)]
+struct Report {
+    levels: Vec<u32>,
+    direction: Direction,
+    safe: bool,
+}
+
+impl Report {
+    fn new(levels: Vec<u32>) -> Report {
+        let first = levels[0];
+        let second = levels[1];
+
+        let mut direction = Direction::Increasing;
+        if first > second {
+            direction = Direction::Decreasing;
+        }
+
+        let mut report = Report {
+            levels,
+            direction,
+            safe: false,
+        };
+
+        &report.safety_check();
+
+        report
+    }
+
+    fn safety_check(&mut self) {
+        self.safe = false;
+        for (idx, level) in self.levels.iter().enumerate() {
+            if idx == self.levels.len() - 1 {
+                break;
+            }
+            let first = self.levels[idx];
+            let second = self.levels[idx + 1];
+            if first < second && self.direction == Direction::Increasing {
+                let diff = first.abs_diff(second);
+                if diff >= 1 && diff <= 3 {
+                    self.safe = true;
+                    continue;
+                }
+            }
+
+            if second < first && self.direction == Direction::Decreasing {
+                let diff = first.abs_diff(second);
+                if diff >= 1 && diff <= 3 {
+                    self.safe = true;
+                    continue;
+                }
+            }
+
+            self.safe = false;
+            break;
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Direction {
+    Increasing,
+    Decreasing,
+}
+
+pub fn process(input: &str) -> anyhow::Result<String> {
+    let reports = input
+        .lines()
+        .map(|line| {
+            let levels = line
+                .split_whitespace()
+                .map(|i| i.parse::<u32>().expect("parse"))
+                .collect::<Vec<_>>();
+            Report::new(levels)
+        })
+        .collect::<Vec<_>>();
+
+    let result = reports.iter().filter(|r| r.safe).count();
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -8,8 +87,13 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!("2", process(input)?);
         Ok(())
     }
 }
