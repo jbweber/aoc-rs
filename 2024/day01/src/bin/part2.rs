4e702186a50df73c0c3a2e9d1aa85dc596b9317e
Jeff Weber <jweber@cofront.net>
2024-12-01 21:53:15 -0500

    change template

--- 2024/day01/src/bin/part2.rs
+++
@@ -1,8 +0,0 @@
-use day01::part2::process;
-
-fn main() -> anyhow::Result<()> {
-    let file = include_str!("../../input1.txt");
-    let result = process(file)?;
-    println!("{}", result);
-    Ok(())
-}
--- 2024/day01/src/part2.rs
+++ 2024/day01/src/part2.rs
@@ -2,14 +2,22 @@ pub fn process(input: &str) -> anyhow::Result<String> {
     let mut col1 = vec![];
     let mut col2 = vec![];
 
-    input.lines().map(|l| l.split_ascii_whitespace().filter_map(|s| s.parse::<u32>().ok()).collect::<Vec<_>>()).for_each(|v| {
-        col1.push(v[0]);
-        col2.push(v[1]);
-    });
+    input
+        .lines()
+        .map(|l| {
+            l.split_ascii_whitespace()
+                .filter_map(|s| s.parse::<u32>().ok())
+                .collect::<Vec<_>>()
+        })
+        .for_each(|v| {
+            col1.push(v[0]);
+            col2.push(v[1]);
+        });
 
-    let result = col1.iter().map(|v| {
-        col2.iter().filter(|&s| v == s).count() as u32 * v
-    }).sum::<u32>();
+    let result = col1
+        .iter()
+        .map(|v| col2.iter().filter(|&s| v == s).count() as u32 * v)
+        .sum::<u32>();
 
     Ok(result.to_string())
 }
