668700a95d91c367ea2f576a4e0ab044e81f45cd
Jeff Weber <jweber@cofront.net>
2024-12-03 04:07:48 -0500

    day 3

--- 2024/day03/src/part2.rs
+++ 2024/day03/src/part2.rs
@@ -1,15 +1,59 @@
-pub fn process(_input: &str) -> anyhow::Result<String> {
-    Ok(String::from("part 2"))
+use regex::Regex;
+
+pub fn process(input: &str) -> anyhow::Result<String> {
+    let result: u32 = process_muls(input).iter().map(|mul| mul.0 * mul.1).sum();
+
+    Ok(result.to_string())
+}
+
+fn process_muls(input: &str) -> Vec<(u32, u32)> {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+
+    let mut on = true;
+    let mut results = Vec::new();
+    for captures in re.captures_iter(input) {
+        if captures.get(0).map(|m| m.as_str()) == Some("do()") {
+            on = true;
+            continue;
+        }
+
+        if captures.get(0).map(|m| m.as_str()) == Some("don't()") {
+            on = false;
+            continue;
+        }
+
+        if !on {
+            continue;
+        }
+
+        let num1: u32 = captures[1].parse().unwrap();
+        let num2: u32 = captures[2].parse().unwrap();
+
+        results.push((num1, num2));
+    }
+
+    results
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("mul(1,3)", vec![(1,3)])]
+    #[case("mul(1,3)mul(1,3)", vec![(1,3),(1,3)])]
+    #[case("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", vec![(2,4),(5,5),(11,8),(8,5)])]
+    #[case("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", vec![(2,4),(8,5)])]
+    fn test_process_muls(#[case] line: &str, #[case] expected: Vec<(u32, u32)>) {
+        let result = process_muls(line);
+        assert_eq!(result, expected)
+    }
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        let input = "";
-        assert_eq!("part 2", process(input)?);
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!("48", process(input)?);
         Ok(())
     }
 }
